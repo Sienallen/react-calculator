f562a614205ade8ec0e4ea832a522c05e718736b
Allen Sien <siena@uci.edu>
2023-12-05 19:34:43 -0800

    some functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,67 @@ import TextBox from './component/TextBox';
 import './App.css';
 
 function App() {
-  const [output, setOutput] = useState('');
+  const [output, setOutput] = useState('0');
+  const [temp, setTemp] = useState('0');
 
   const clickHandler = (text: string) => {
     setOutput(text);
   };
+
+  const combineText = (text: string) => {
+    setOutput(output !== '0' ? output + text : text);
+  };
+
+  const moveString = () => {
+    setTemp(output);
+    setOutput('0');
+  };
+
   return (
     <>
       <div>
+        <TextBox text={temp} name="temp"></TextBox>
         <TextBox text={output} name="output"></TextBox>
         <div className="btn-grid">
           <Button text={'%'} onClick={clickHandler} />
-          <Button text={'CE'} onClick={clickHandler} />
-          <Button text={'C'} onClick={clickHandler} />
-          <Button text={'<-'} onClick={clickHandler} />
-          <Button text={'⅛'} onClick={clickHandler} />
-          <Button text={'x²'} onClick={clickHandler} />
-          <Button text={'√x'} onClick={clickHandler} />
-          <Button text={'÷'} onClick={clickHandler} />
-          <Button text={'7'} onClick={clickHandler} />
-          <Button text={'8'} onClick={clickHandler} />
-          <Button text={'9'} onClick={clickHandler} />
-          <Button text={'x'} onClick={clickHandler} />
-          <Button text={'4'} onClick={clickHandler} />
-          <Button text={'5'} onClick={clickHandler} />
-          <Button text={'6'} onClick={clickHandler} />
-          <Button text={'-'} onClick={clickHandler} />
-          <Button text={'1'} onClick={clickHandler} />
-          <Button text={'2'} onClick={clickHandler} />
-          <Button text={'3'} onClick={clickHandler} />
-          <Button text={'+'} onClick={clickHandler} />
+          <Button text={'CE'} onClick={() => setOutput('0')} />
+          <Button
+            text={'C'}
+            onClick={() => {
+              setOutput('0');
+              setTemp('0');
+            }}
+          />
+          <Button text={'<-'} onClick={() => setOutput(output.slice(0, -1))} />
+          <Button
+            text={'1/x'}
+            onClick={() => setOutput((1 / parseFloat(output)).toString())}
+          />
+          <Button
+            text={'x²'}
+            onClick={() =>
+              setOutput(Math.pow(parseFloat(output), 2).toString())
+            }
+          />
+          <Button
+            text={'√x'}
+            onClick={() => setOutput(Math.sqrt(parseFloat(output)).toString())}
+          />
+          <Button text={'÷'} onClick={moveString} />
+          <Button text={'7'} onClick={combineText} />
+          <Button text={'8'} onClick={combineText} />
+          <Button text={'9'} onClick={combineText} />
+          <Button text={'x'} onClick={moveString} />
+          <Button text={'4'} onClick={combineText} />
+          <Button text={'5'} onClick={combineText} />
+          <Button text={'6'} onClick={combineText} />
+          <Button text={'-'} onClick={moveString} />
+          <Button text={'1'} onClick={combineText} />
+          <Button text={'2'} onClick={combineText} />
+          <Button text={'3'} onClick={combineText} />
+          <Button text={'+'} onClick={moveString} />
           <Button text={'+/-'} onClick={clickHandler} />
-          <Button text={'0'} onClick={clickHandler} />
+          <Button text={'0'} onClick={combineText} />
           <Button text={'.'} onClick={clickHandler} />
           <Button text={'='} onClick={clickHandler} />
         </div>
