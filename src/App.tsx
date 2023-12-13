3580db7a91e95af5cb1762f9192e53eb3498c657
Allen Sien <siena@uci.edu>
2023-12-13 14:33:44 -0800

    added history section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Button from './component/Button';
 import TextBox from './component/TextBox';
 import './App.css';
+
+//https://www.youtube.com/watch?v=RZ5wKYbOM_I
 
 function App() {
   const [output, setOutput] = useState('0');
@@ -9,15 +11,7 @@ function App() {
   const [operator, setOperator] = useState('');
   const [reset, setReset] = useState(false);
   const [expression, setExpression] = useState('');
-
-  /* useEffect(() => {
-    console.log(expression);
-    if (operator === '') {
-      setExpression(output);
-    } else if (operator != '') {
-      setExpression(expression + operator);
-    }
-  }, []); */
+  const [history, setHistory] = useState<string[]>([]);
 
   // changes output that displays when pressing buttons 0-9
   const clickHandler = (text: string) => {
@@ -28,19 +22,15 @@ function App() {
     if (output.charAt(output.length - 1) !== text) {
       setOutput(output + text);
       setReset(false);
-      console.log('period: ' + reset);
     }
   };
 
   //adds 0-9 to output string that displays
   const combineText = (text: string) => {
     if (reset) {
-      console.log('insde reset');
       setTemp(output);
       setOutput(text);
       setReset(false);
-      console.log('combineText:' + reset);
-      /* setExpression(text); */
     } else {
       setOutput(output !== '0' ? output + text : text);
     }
@@ -51,125 +41,149 @@ function App() {
       setTemp(output);
       setOutput('0');
       setOperator(symbol);
-      setExpression(output + symbol);
+      setExpression(output + ' ' + symbol);
       setReset(false);
-      console.log('moveString:' + reset);
     } else {
-      console.log('going to solve');
       /* solve(); */
-      setExpression(solve() + symbol);
+      let solved = solve();
+      setExpression(solved + ' ' + symbol);
       setOperator(symbol);
+      setTemp(solved);
+      setOutput(solved);
     }
   };
 
+  const addHistory = (newHistory: string) => {
+    setHistory([newHistory, ...history]);
+  };
+
   const solve = () => {
+    let newHistory = expression + ' ' + output + ' = ';
     setExpression(
       output != expression.substring(expression.length - output.length)
-        ? expression + output + '='
-        : expression + '='
+        ? newHistory
+        : expression + ' ='
     );
     let tempOutput: string = '';
     switch (operator) {
       case '+': {
         tempOutput = (parseFloat(temp) + parseFloat(output)).toString();
-        setOutput((parseFloat(temp) + parseFloat(output)).toString());
         break;
       }
       case '-': {
         tempOutput = (parseFloat(temp) - parseFloat(output)).toString();
-        setOutput((parseFloat(temp) - parseFloat(output)).toString());
         setOperator('');
         break;
       }
       case 'x': {
         tempOutput = (parseFloat(temp) * parseFloat(output)).toString();
-        setOutput((parseFloat(temp) * parseFloat(output)).toString());
         break;
       }
       case '÷': {
         tempOutput = (parseFloat(temp) / parseFloat(output)).toString();
-        setOutput((parseFloat(temp) / parseFloat(output)).toString());
         break;
       }
       case '': {
         tempOutput = output;
-        setOutput(output);
         break;
       }
     }
+    newHistory = newHistory + output;
     setReset(true);
-    console.log('solve: ' + reset);
     setOperator('');
+    addHistory(newHistory);
     return tempOutput;
   };
 
   return (
     <>
-      <div>
-        <TextBox text={expression} name="temp"></TextBox>
-        <TextBox text={output} name="output"></TextBox>
-        <div className="btn-grid">
-          <Button text={'%'} onClick={clickHandler} />
-          <Button
-            text={'CE'}
-            onClick={() => {
-              setOutput('0');
-              setExpression(
-                expression.substring(0, expression.length - output.length)
-              );
-            }}
-          />
-          <Button
-            text={'C'}
-            onClick={() => {
-              setOutput('0');
-              setTemp('0');
-              setExpression('');
-              setReset(false);
-              console.log('C: ' + reset);
-              setOperator('');
-            }}
-          />
-          <Button text={'<-'} onClick={() => setOutput(output.slice(0, -1))} />
-          <Button
-            text={'1/x'}
-            onClick={() => setOutput((1 / parseFloat(output)).toString())}
-          />
-          <Button
-            text={'x²'}
-            onClick={() =>
-              setOutput(Math.pow(parseFloat(output), 2).toString())
-            }
-          />
-          <Button
-            text={'√x'}
-            onClick={() => setOutput(Math.sqrt(parseFloat(output)).toString())}
-          />
-          <Button text={'÷'} onClick={moveString} />
-          <Button text={'7'} onClick={combineText} />
-          <Button text={'8'} onClick={combineText} />
-          <Button text={'9'} onClick={combineText} />
-          <Button text={'x'} onClick={moveString} />
-          <Button text={'4'} onClick={combineText} />
-          <Button text={'5'} onClick={combineText} />
-          <Button text={'6'} onClick={combineText} />
-          <Button text={'-'} onClick={moveString} />
-          <Button text={'1'} onClick={combineText} />
-          <Button text={'2'} onClick={combineText} />
-          <Button text={'3'} onClick={combineText} />
-          <Button text={'+'} onClick={moveString} />
-          <Button
-            text={'+/-'}
-            onClick={() => {
-              setOutput(
-                parseFloat(output) < 0 ? output.substring(1) : '-' + output
-              );
-            }}
-          />
-          <Button text={'0'} onClick={combineText} />
-          <Button text={'.'} onClick={period} />
-          <Button text={'='} onClick={solve} />
+      <div className="flexBox">
+        <div className="calculator">
+          <TextBox text={expression} name="temp"></TextBox>
+          <TextBox text={output} name="output"></TextBox>
+          <div className="btn-grid">
+            <Button text={'%'} onClick={clickHandler} />
+            <Button
+              text={'CE'}
+              onClick={() => {
+                setOutput('0');
+                setExpression(
+                  expression.substring(0, expression.length - output.length)
+                );
+              }}
+            />
+            <Button
+              text={'C'}
+              onClick={() => {
+                setOutput('0');
+                setTemp('0');
+                setExpression('');
+                setReset(false);
+                setOperator('');
+              }}
+            />
+            <Button
+              text={'<-'}
+              onClick={() => setOutput(output.slice(0, -1))}
+            />
+            <Button
+              text={'1/x'}
+              onClick={() => setOutput((1 / parseFloat(output)).toString())}
+            />
+            <Button
+              text={'x²'}
+              onClick={() =>
+                setOutput(Math.pow(parseFloat(output), 2).toString())
+              }
+            />
+            <Button
+              text={'√x'}
+              onClick={() =>
+                setOutput(Math.sqrt(parseFloat(output)).toString())
+              }
+            />
+            <Button text={'÷'} onClick={moveString} />
+            <Button text={'7'} onClick={combineText} />
+            <Button text={'8'} onClick={combineText} />
+            <Button text={'9'} onClick={combineText} />
+            <Button text={'x'} onClick={moveString} />
+            <Button text={'4'} onClick={combineText} />
+            <Button text={'5'} onClick={combineText} />
+            <Button text={'6'} onClick={combineText} />
+            <Button text={'-'} onClick={moveString} />
+            <Button text={'1'} onClick={combineText} />
+            <Button text={'2'} onClick={combineText} />
+            <Button text={'3'} onClick={combineText} />
+            <Button text={'+'} onClick={moveString} />
+            <Button
+              text={'+/-'}
+              onClick={() => {
+                setOutput(
+                  parseFloat(output) < 0 ? output.substring(1) : '-' + output
+                );
+              }}
+            />
+            <Button text={'0'} onClick={combineText} />
+            <Button text={'.'} onClick={period} />
+            <Button text={'='} onClick={solve} />
+          </div>
         </div>
+
+        {
+          <div className="historyBox">
+            <h2>History</h2>
+            {history.map((data) => {
+              return (
+                <Button
+                  text={data}
+                  onClick={() => {
+                    console.log(data);
+                  }}
+                />
+              );
+            })}
+          </div>
+        }
       </div>
     </>
   );
